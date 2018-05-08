6b23da1779620b19317246380a02c893d7c6e7bf
AlfredJM <alfredjm.02@gmail.com>
2018-05-08 17:47:12 -0400

    Eliminada funcion innecesaria de la quiniela

--- src/main/java/com/webq/quiniela/controlador/IndexControlador.java
+++ src/main/java/com/webq/quiniela/controlador/IndexControlador.java
@@ -3,19 +3,13 @@ package com.webq.quiniela.controlador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class IndexControlador {
 	
-	@GetMapping("/")
+	@GetMapping({"/","/index","/home"})
 	public String inicio(Model model) {
 		model.addAttribute("saludo","Hello!!");
 		return "index";
-	}
-	@GetMapping("/quiniela")
-	public String quiniela(Model model) {
-		return "crearquiniela";
 	}
 }
